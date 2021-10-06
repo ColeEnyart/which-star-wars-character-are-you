300e42c7bcda1720e8d5e249000885b3608568cb
Damy Osinubi <damyosinubi@Damys-MacBook-Pro.local>
2021-10-06 10:26:07 -0500

    added an id element in index.html and added a .js file

---
+++ assets/js/dami.js
@@ -0,0 +1,43 @@
+
+
+
+
+var buttonPress = document.getElementById("blastoff")
+console.log (buttonPress)
+
+
+
+//unction getRandomValue(arr) {
+   // var result = arr[Math.floor(Math.random() * arr.length)];
+  //  return result;
+  
+
+
+
+
+function getRandomCharacter(event){
+    event.preventDefault()
+    for (var i = 1; i < 2; i++)
+        var requestPeopleURl = "https://swapi.dev/api/people/" + i;
+
+        fetch(requestPeopleURl)
+            .then(function (response){
+                return response.json()
+            })
+            .then( function (data) {
+                 console.log(data);
+            })
+             
+
+
+}
+
+
+
+
+
+buttonPress.addEventListener("click", getRandomCharacter);
+
+    
+
+
