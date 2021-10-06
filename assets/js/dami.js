0ad40e6074947095a67e53c16f8b11722ca8afa1
Damy Osinubi <damyosinubi@Damys-MacBook-Pro.local>
2021-10-06 13:19:07 -0500

    Resolved random character extraction

--- assets/js/dami.js
+++ assets/js/dami.js
@@ -3,42 +3,66 @@
 
 
 var buttonPress = document.getElementById("blastoff")
-console.log (buttonPress)
+// console.log (buttonPress)
+
+var keep2 = []
+
+//function getRandomValue(arr) {
+   // 
 
 
 
-//unction getRandomValue(arr) {
-   // var result = arr[Math.floor(Math.random() * arr.length)];
-  //  return result;
+
+  function getRandomCharacter(event){
+    event.preventDefault()
+    for (var i = 1; i < keep2.length ; i++){
+        var result = keep2[Math.floor(Math.random() * keep2.length)];
+         console.log(result)
+        return result;
   
 
-
-
-
-function getRandomCharacter(event){
-    event.preventDefault()
-    for (var i = 1; i < 2; i++)
-        var requestPeopleURl = "https://swapi.dev/api/people/" + i;
-
-        fetch(requestPeopleURl)
-            .then(function (response){
-                // return response.json()
-                return response[Math.floor(Math.random()*arr.length)];
-            })
-            .then( function (data) {
-                 console.log(data);
-            })
-             
+    }
+        
 
 
 }
+
+
+function testApi2() {
+    for (var i = 1; i < 83; i++) {
+
+        var queryUrl = "https://swapi.dev/api/people/" + i;
+
+        fetch(queryUrl)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                keep2.push(data);
+                /* console.log(data) */
+                // $("table").append('<tr><td>' + ti + '</td></tr>');
+            })
+    }
+}
+console.log(keep2);
+
+
+testApi();
+testApi2();
+
+
+
+
+
+
+
+
+
+
 
 
 
 
 
 buttonPress.addEventListener("click", getRandomCharacter);
-
-    
-
 
