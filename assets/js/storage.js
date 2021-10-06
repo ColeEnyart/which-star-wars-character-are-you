996b71e38080d3e25c418bdc7646aa5084b5c274
Cole Enyart <letchro@gmail.com>
2021-10-06 18:57:02 -0500

    Added function to output localStorage into webpage

--- assets/js/storage.js
+++ assets/js/storage.js
@@ -1,14 +1,3 @@
-var characterNameEl = $(".name");
+var characterNameEl = $(".name").val();
 var characterImgEl = $(".card-img-top");
 
-function displaySearchHistory() {
-    const key = 'Search History';
-    var searchHistory = JSON.parse(localStorage.getItem(key) || "[]");
-    var buttons = [];
-
-    for (var i = 0; i < searchHistory.length; i++) {
-        buttons.push('<button class="btn btn-secondary col-12 addedButton">' + searchHistory[i] + '</button>');
-    }
-
-    $('.searchHistory').html(buttons.join(""));
-}
